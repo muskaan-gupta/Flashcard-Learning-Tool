3efa36524d76f360ecb5f716a3f701ed2b0d597d
Muskan Gupta <145460146+muskaan-gupta@users.noreply.github.com>
2025-05-05 23:07:11 +0530

    testing the hardcoded api uri

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `${process.env.API_URI}/api/v1`,
+  baseURL: `https://flashlearn-api.onrender.com/api/v1`,
   headers: {
     "Content-Type": "application/json",
   },
