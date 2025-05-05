a15cee4b4c9100aba1c111061acacaba82feed43
Muskan Gupta <145460146+muskaan-gupta@users.noreply.github.com>
2025-05-05 17:50:19 +0000

    resetted the API uri to fetch from env

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+
 const axiosInstance = axios.create({
-  baseURL: `https://flashlearn-api.onrender.com/api/v1`,
+  baseURL: `${import.meta.env.VITE_API_URI}/api/v1`,
   headers: {
     "Content-Type": "application/json",
   },
